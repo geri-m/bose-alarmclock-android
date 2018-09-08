fcb906360dba2bde205723e7dd7366b91f7096e9
Gerald Madlmayr <gerald.madlmayr@gmx>
2018-09-09 01:03:22 +0200

    Beautification of Logic

--- app/src/main/java/at/madlmayr/bosealarmclock/PowerButtonAsyncTask.java
+++ app/src/main/java/at/madlmayr/bosealarmclock/PowerButtonAsyncTask.java
@@ -4,7 +4,6 @@ import android.util.Log;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.xml.XmlHttpContent;
@@ -18,19 +17,16 @@ import at.madlmayr.bosealarmclock.key.KeyRequest;
 import at.madlmayr.bosealarmclock.key.KeyResponse;
 import at.madlmayr.bosealarmclock.key.KeyStateEnum;
 import at.madlmayr.bosealarmclock.nowplaying.NowPlayingResponse;
+import at.madlmayr.bosealarmclock.nowplaying.SourceEnum;
 
 public class PowerButtonAsyncTask extends AbstractAsyncTask {
 
-    // We don't have a namespace for our Application.
-    // Currently not in use.
-    static final XmlNamespaceDictionary DICTIONARY = new XmlNamespaceDictionary().set("", "");
+    // We don't have a namespace for our Application and/or the objects. Soundtouch doesn't like it.
+    private static final XmlNamespaceDictionary DICTIONARY = new XmlNamespaceDictionary().set("", "");
 
-
-    // For Local Debugging of the XML Request I use a netcat, as there is very little docu on
-    // the new Java HTTP Client from GOOGLE.
+    // For Local Testing.
     // private static final String BASE_PATH = "http://geralds-mbp.fritz.box:1234/";
-    // Static URL to my soundbar. Yes I'm owning a Fritzbox ;)
-
+    // Static URL to my SoundTouch. Yes I'm owning a Fritzbox ;)
     private static final String BASE_PATH = "http://soundtouch-20.fritz.box:8090/";
     private final HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
     private final GenericUrl postKey = new GenericUrl(BASE_PATH + "key");
@@ -41,49 +37,23 @@ public class PowerButtonAsyncTask extends AbstractAsyncTask {
     @Override
     protected void doInBackground() throws IOException {
 
-        try {
+        // https://github.com/google/google-http-java-client/blob/dev/google-http-client-xml/src/test/java/com/google/api/client/xml/XmlTest.java
+        final NowPlayingResponse nowPlayingResponse = factory.buildGetRequest(getNowPlaying).setParser(new XmlObjectParser(DICTIONARY)).execute().parseAs(NowPlayingResponse.class);
 
-            // https://github.com/google/google-http-java-client/blob/dev/google-http-client-xml/src/test/java/com/google/api/client/xml/XmlTest.java
-            final HttpRequest nowPlayingRequest = factory.buildGetRequest(getNowPlaying);
-            nowPlayingRequest.getHeaders().setAcceptEncoding("gzip, deflate").setAccept("*/*");
-            nowPlayingRequest.setParser(new XmlObjectParser(DICTIONARY));
-            final NowPlayingResponse nowPlayingResponse = nowPlayingRequest.execute().parseAs(NowPlayingResponse.class);
-            Log.i(MainActivity.TAG, nowPlayingResponse.getContentItem().getSource().toString());
+        // If we are in Standby, start the SoundTouch
+        if (nowPlayingResponse.getContentItem().getSource().equals(SourceEnum.STANDBY)) {
+            Log.i(MainActivity.TAG, "SoundTouch in Standby, Power on");
 
             final XmlHttpContent contentPowerStep1 = new XmlHttpContent(DICTIONARY, KeyRequest.ELEMENT_NAME, new KeyRequest(KeyPressAndReleaseEnum.POWER.name(), KeyStateEnum.PRESS.getValue()));
-            final HttpRequest requestStep1 = factory.buildPostRequest(postKey, contentPowerStep1);
-            requestStep1.getHeaders().setContentType("text/xml").setAcceptEncoding("gzip, deflate").setAccept("*/*");
-            requestStep1.setParser(new XmlObjectParser(DICTIONARY));
-            final KeyResponse keyResponseStep1 = requestStep1.execute().parseAs(KeyResponse.class);
+            final KeyResponse keyResponseStep1 = factory.buildPostRequest(postKey, contentPowerStep1).setParser(new XmlObjectParser(DICTIONARY)).execute().parseAs(KeyResponse.class);
             Log.i(MainActivity.TAG, keyResponseStep1.getValue());
 
             final XmlHttpContent contentPowerStep2 = new XmlHttpContent(DICTIONARY, KeyRequest.ELEMENT_NAME, new KeyRequest(KeyPressAndReleaseEnum.POWER.name(), KeyStateEnum.RELEASE.getValue()));
-            final HttpRequest requestStep2 = factory.buildPostRequest(postKey, contentPowerStep2);
-            requestStep2.getHeaders().setContentType("text/xml").setAcceptEncoding("gzip, deflate").setAccept("*/*");
-            requestStep2.setParser(new XmlObjectParser(DICTIONARY));
-            final KeyResponse keyResponseStep2 = requestStep2.execute().parseAs(KeyResponse.class);
+            final KeyResponse keyResponseStep2 = factory.buildPostRequest(postKey, contentPowerStep2).setParser(new XmlObjectParser(DICTIONARY)).execute().parseAs(KeyResponse.class);
             Log.i(MainActivity.TAG, keyResponseStep2.getValue());
-
-
-            // Bose's API Document is not correct. It is sufficient to send a single "press" for the Power Button.
-            // We are sending Text that is handcrafted for the beginning, which is sufficient.
-/*
-            if (infoResponse.parseAsString().contains("STANDBY")) {
-
-                Log.i(TAG, "Box is in standby, Power on");
-
-                final HttpRequest request = factory.buildPostRequest(postKey, ByteArrayContent.fromString(null, "<key state=\"press\" sender=\"Gabbo\">POWER</key>"));
-                request.getHeaders().setContentType("text/xml").setAcceptEncoding("gzip, deflate").setAccept("*//*");
-                final HttpResponse response = request.execute();
-                Log.i(TAG, response.getStatusMessage());
-            } else {
-                Log.i(TAG, "Box is already running");
-            }
-*/
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
+            Log.i(MainActivity.TAG, "SoundTouch already started");
         }
-
 
     }
 }
