f695558b2596be258f5090f4b36363101d63b6ea
Gerald Madlmayr <gerald.madlmayr@gmx>
2018-09-09 11:43:15 +0200

    Adding Code from Sample

---
+++ app/src/main/java/at/madlmayr/bosealarmclock/AlarmReceiver.java
@@ -0,0 +1,23 @@
+package at.madlmayr.bosealarmclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        // TODO: Replace with Calls to Bose's SoundTouch
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        final Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+}
--- app/src/main/java/at/madlmayr/bosealarmclock/MainActivity.java
+++ app/src/main/java/at/madlmayr/bosealarmclock/MainActivity.java
@@ -1,26 +1,72 @@
 package at.madlmayr.bosealarmclock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     static final String TAG = "BoseAlarmClock";
 
+    private TimePicker alarmTimePicker;
+    private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate");
+        alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Log.i(TAG, "onCreate successfully completed");
     }
 
+    /**
+     * Callback from the GUI when pressing the "Power" button
+     *
+     * @param view View passed from the GUI
+     */
     public void pressPower(final View view) {
         Log.i(TAG, "pressPower");
         new PowerButtonAsyncTask().execute();
-
-
     }
+
+    /**
+     * Callback from the GUI when pressing the "Toggle" button
+     *
+     * @param view View passed from the GUI
+     */
+    public void onToggleClicked(final View view) {
+        long time;
+        if (((ToggleButton) view).isChecked()) {
+            Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent intent = new Intent(this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+            time = (calendar.getTimeInMillis() - (calendar.getTimeInMillis() % 60000));
+            if (System.currentTimeMillis() > time) {
+                if (calendar.AM_PM == 0)
+                    time = time + (1000 * 60 * 60 * 12);
+                else
+                    time = time + (1000 * 60 * 60 * 24);
+            }
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
